7fde12c8716d20c0da82148878ce830fd8682db0
andreyyy178 <61029884+andreyyy178@users.noreply.github.com>
2021-12-03 16:53:20 -0500

    commit

---
+++ src/main/java/com/codepath/couple/ProfileURLString.java
@@ -0,0 +1,11 @@
+package com.codepath.couple;
+
+public class ProfileURLString {
+   public static String url;
+//   public static ProfileURLString stringURLInstance = new ProfileURLString();
+//
+//   public static void setUrl (String url1){
+//      url = url1;
+//   }
+}
+
